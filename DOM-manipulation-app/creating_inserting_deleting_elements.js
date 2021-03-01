7e4d1d1ec07dac9b4a4df8f5d57ed224995d43c0
Rupak Lama <rlama@uptopcorp.com>
2021-02-28 19:42:10 -0500

    selecting, creating, inserting and deleting elements

---
+++ DOM-manipulation-app/creating_inserting_deleting_elements.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// CREATING ELEMENTS
+
+// DOM createElement() Method
+// The createElement() method creates an Element Node with the specified name.
+// Tip: After the element is created,
+// use the element.appendChild() or element.insertBefore() method to insert it to the document.
+const message = document.createElement('div'); // create div element
+
+// NOTE: now we can add classes to this new div element with classList property
+// This property is useful to 'add, remove and toggle CSS classes' on an element.
+// The classList property is read-only, however, you can modify it by using the add() and remove() methods.
+message.classList.add('cookie-message');
+
+// textContent - simply inserts plain text
+message.textContent = 'We use cookies to improve functionality and analytics.';
+
+// innerHTML - to insert HTML
+message.innerHTML =
+  'We use cookies to improve functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+const header = document.querySelector('.header');
+
+// Tip: After the element is created,
+// use the element.prepend or append method to insert it to the document.
+
+// INSERTING ELEMENTS
+// prepend() method inserts DOMString objects as Text nodes
+// prepend() method adds an element as a 'first child' of the parent element - header here
+header.prepend(message);
+
+// NOTE: we can also append as a 'last child' element
+header.append(message);
+
+// NOTE - before() method is used to insert element or text before the given element
+// after() method is used to insert element or text after the given element
+header.before(message);
+header.after(message);
+
+// NOTE - DELETE ELEMENTS, remove()
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+  // old way of doing same thing with DOM traversing
+  // message.parentElement.removeChild(message);
+});
+
+// NOTE:
+// 'innerHTML' parses content as HTML, so it takes longer.
+// 'nodeValue' uses straight text, does not parse HTML, and is faster.
+// 'textContent' uses straight text, does not parse HTML, and is faster.
+// 'innerText' Takes styles into consideration. It won't get hidden text for instance.
---
+++ DOM-manipulation-app/selecting_elements.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// Selecting Elements
+
+// selecting top html element - document
+// documentElement - to select Entire Document of any WEB PAGE because
+// document is window object, not an html element
+// NOTE: In the window object, select ENTIRE WEB PAGE
+console.log(document.documentElement); // <html>...</html>, selects entire web page
+// NOTE: If we want to apply CSS for the entire page, we always need to select 'documentElement'
+
+// Note: we can also select specific elements in a page
+// NOTE: In the window object, select HEAD Element
+console.log(document.head);
+
+// body
+console.log(document.body);
+
+// NOTE: FOR ALL ABOVE ELEMENTS WE DON'T EVEN NEED TO WRITE ANY SELECTORS like querySelector()
+
+// NOTE: DOM querySelector() Method is not only available on DOCUMENT (document.)
+// but also on ALL THE ELEMENTS, frequently use to select CHILD elements
+
+// DOM querySelector() Method
+// querySelector() method returns the first element that matches a specified CSS selector(s) in the document
+// Note: The querySelector() method only returns the first element that matches the specified selectors.
+// To return all the matches, use the querySelectorAll() method instead - Array of Node Elements
+console.log(document.querySelector('.header')); // <header class="header"></header>
+
+// NodeList(4) [section#section--1.section, section#section--2.section, section#section--3.section,
+// section.section.section--sign-up]
+const allSections = document.querySelectorAll('.section'); // all sections
+console.log(allSections);
+
+// DOM getElementById() Method
+// getElementById() method returns the element that has the ID attribute with the specified value
+// NOTE: we don't need CSS Selectors here like ('.', '#') those are only for querySelector() Method
+console.log(document.getElementById('section--1'));
+
+// DOM getElementsByTagName() Method
+// Get all elements in the document with the specified tag name
+// The getElementsByTagName() method returns a collection of all elements in the document
+// with the specified tag name, as an HTMLCollection object - Array of Nodes
+
+// The HTMLCollection object represents a collection of nodes. - Array of Nodes
+// The nodes can be accessed by index numbers. The index starts at 0.
+// To get all the buttons - all the elements with the name of 'button'
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+// DOM getElementsByClassName() Method
+// The getElementsByClassName() method returns a collection of all elements in the document
+// with the specified class name, as an HTMLCollection object  - Array of Nodes
+// The HTMLCollection object represents a collection of nodes. - Array of Nodes
+// The nodes can be accessed by index numbers. The index starts at 0.
+console.log(document.getElementsByClassName('btn'));
