d9872b79b1b83b45de36542f72fd9611485ba4eb
Rupak Lama <62438399+rupaklama@users.noreply.github.com>
2021-06-30 22:59:47 -0400

    Update array.js

--- data_structures/array.js
+++ data_structures/array.js
@@ -1,12 +1,13 @@
 'use strict';
 
-// regular way of array destructuring
+// regular way of creating variables from the array items with bracket notation
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-// ES6 data destructuring
+// ES6 array destructuring
+// Array destructuring allows you to create new variables using an array item as a value.
 const [x, y, z] = arr;
 console.log(x, y, z); // 2 3 4
 
