5290de301f778af69f94c3870852f9b120bab5b0
Rupak Lama <62438399+rupaklama@users.noreply.github.com>
2021-10-31 11:16:29 -0400

    Update find.js

--- array_app/find.js
+++ array_app/find.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// find() method returns the value of the first element in an array that pass a test (provided as a function)
+// find() method is to get ONE element in an array that pass a test condition (provided as a function) 
 // find() method executes the function once for each element present in the array
 // If it finds an array element where the function returns a true value,
 // find() returns the value of that array element (and does not check the remaining values)
