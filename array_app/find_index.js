1943b968a41ca4538aeeaea7cca03c46171549a3
Rupak Lama <62438399+rupaklama@users.noreply.github.com>
2021-11-07 11:06:14 -0500

    Update find_index.js

--- array_app/find_index.js
+++ array_app/find_index.js
@@ -8,7 +8,24 @@
 
 // NOTE: find() is lot similar to find method which returns an 'Element' &
 // findIndex() method returns the index of the first element
+if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    // NOTE: findIndex() is lot similar to Find method which returns an 'Element' &
+    // findIndex() method returns the index of the first element
 
-// indexOf() & findIndex() does the same thing
-// NOTE: the difference is indexOf expects a 'value' as first parameter
-// findIndex() expects a 'callback' as first parameter
+    // indexOf() & findIndex() does the same thing
+    // NOTE: the difference is indexOf expects a 'value' as first parameter
+    // .indexOf(23) - if an array contains 23 then true else false
+    // findIndex() expects a 'callback' as first parameter
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+
+    // SPLICE method - splice() method removes items to an array, and returns the removed item(s) also
+    // Note: This method works as same as the SLICE method but changes/mutates the ORIGINAL ARRAY
+    // In practice, we use Splice to delete one or more elements in an ORIGINAL ARRAY
+    accounts.splice(index, 1); // index where 1 item is deleted
+  }
+});
