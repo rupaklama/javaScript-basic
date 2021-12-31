d9114e043033804aca50fa6d9c5c42f1e9d42284
Rupak Lama <62438399+rupaklama@users.noreply.github.com>
2021-12-31 12:04:26 -0500

    Update function.js

--- javaScript-basic2/function.js
+++ javaScript-basic2/function.js
@@ -3,47 +3,60 @@
 
 // note - Function is just a reusable piece of code store in a global variable.
 
+// function Statements/Declarations & Expressions
+// note - when it comes to JS Statement & Expression, it is not same with Functions
+// In Function - it is how functions are named: Function Declaration/Keyword vs Function stored in a Variable
+
+// The main difference between a function expression and a
+// function declaration/statement is the function name - Function Declaration/Keyword vs Function stored in a Variable. 
+// Function stored in a Variable is to create Anonymous functions.
+
+// NOTE - The other big difference is Function Declaration are Hoisted
+// meaning we can call it before/above where they are defined. 
+
+// Function Declaration
+function calcAge(birthYear) {
+  return 2021 - birthYear
+}
+
+// Function Expression - Anonymous function here
+// note - Arrow function is also Function Expression
+let whatDoYouDo = function(job, firstName) {
+  switch(job) {
+    // return doesn't only return value but also finish execution like break statement
+    case 'teacher':
+      return firstName + ' teaches kids how to code';
+    case ' driver':
+      return firstName + ' drives a cab';
+    case 'developer':
+      return firstName + ' creates application';
+    default:
+      return firstName + ' does something else';
+  }
+}
+
+console.log(whatDoYouDo('developer', 'Rupak'))
+
 // NOTE: ARROW FUNCTION
 const name = 'Rupak';
 
 let me = {
-  name: 'Ashutosh Verma',
+  name: "Ashutosh Verma",
   thisInArrow: () => {
-    console.log('My name is ' + name); // no 'this' binding here
-    // NOTE - ES6 arrow function can't be bound a 'this' keyword,
-    // so it will lexically go up a scope & find the value
+    // console.log("My name is " + this.name); - adding 'this' here, no Variable will be found or value,
+    // also no errors thrown
+    console.log("My name is " + name); // no binding of 'this' here to the Object
+    // NOTE - ES6 arrow function can't be bound with a 'this' keyword,
+    // so it will Lexically go up a scope & find the value
   },
   thisInRegular() {
-    console.log('My name is ' + this.name); // 'this' binding works here
+    console.log("My name is " + this.name); // 'this' binding works here
   },
 };
-me.thisInArrow();
-me.thisInRegular();
+me.thisInArrow(); // Rupak
+me.thisInRegular(); // Ashutosh Verma
 
-// Statement doesn't return value while Expression returns/produces value
-// function expression produce immediate result, function declaration/statement don't
 
-// function Statements/Declarations & Expressions
-
-// The main difference between a function expression and a
-// function declaration/statement is the function name,
-// which can be omitted in function expressions to create anonymous functions.
-
-// let whatDoYouDo = function(job, firstName) {
-//   switch(job) {
-//     // return doesn't only return value but also finish execution like break statement
-//     case 'teacher':
-//       return firstName + ' teaches kids how to code';
-//     case ' driver':
-//       return firstName + ' drives a cab';
-//     case 'developer':
-//       return firstName + ' creates application';
-//     default:
-//       return firstName + ' does something else';
-//   }
-// }
-
-// console.log(whatDoYouDo('developer', 'Rupak'))
 
 // A JavaScript function can also be defined using an expression.
 // A function expression can be stored in a variable:
