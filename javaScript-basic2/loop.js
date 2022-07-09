9c9529ce8f38199f7a5c0272b85dc785563acd57
Rupak Lama <62438399+rupaklama@users.noreply.github.com>
2022-07-09 09:28:43 -0400

    Update loop.js

--- javaScript-basic2/loop.js
+++ javaScript-basic2/loop.js
@@ -77,4 +77,16 @@ while (i <= 10) { // counter condition
   i++; //counter increment
 }
 
+// Both for..of and for..in statements iterate over lists;
+// the values iterated on are different though, for..in returns a list of keys on the object being iterated,
+// whereas for..of returns a list of values of the numeric properties of the object being iterated.
+let list = [4, 5, 6];
+
+for (let i in list) {
+  console.log(i); // 0  1 2
+}
+
+for (let i of list) {
+  console.log(i); // 4 5 6
+}
 
