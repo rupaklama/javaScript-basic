9a9839b230171f24af085a4ee90dcd645aec9ce6
Rupak Lama <62438399+rupaklama@users.noreply.github.com>
2022-07-18 11:14:14 -0400

    Update find.js

--- array_app/find.js
+++ array_app/find.js
@@ -33,7 +33,7 @@ const account2 = {
 const accounts = [account1, account2];
 
 // NOTE - using find method, we can find an object in our array based on the some property of that object
-//  to Find particular WHOLE Object on matching certain property
+//  If not found, returns 'undefined'
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account); // {owner: "Jessica Davis", movements: Array(8), interestRate: 1.5, pin: 2222}
 
